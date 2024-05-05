ca99e63dcea211fb490400b8b04329882000e188
AirCairo <103526307+AirCairo@users.noreply.github.com>
2024-05-05 17:59:58 -0400

    Display list of people

---
+++ src/app/mock-peoples.ts
@@ -0,0 +1,13 @@
+import { Person } from './person';
+
+export const PERSONS: Person[] = [
+    { id: 12, name: 'William Joseph Sanders' },
+    { id: 13, name: 'Al Bradd' },
+    { id: 14, name: 'Robert Ashe' },
+    { id: 15, name: 'Steve Simpson' },
+    { id: 16, name: 'Danny Bond' },
+    { id: 17, name: 'Art Bond' },
+    { id: 18, name: 'Scott Bond' },
+    { id: 19, name: 'Carolyn Simpson' },
+    { id: 20, name: 'Sean Miles' }
+]
--- src/app/peoples/peoples.component.ts
+++ src/app/peoples/peoples.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { Person } from '../person';
+import { PERSONS } from '../mock-peoples';
 
 @Component({
   selector: 'app-peoples',
   templateUrl: './peoples.component.html',
-  styleUrl: './peoples.component.css'
+  styleUrl: './peoples.component.css',
+  
+
 })
+
+
 export class PeoplesComponent {
-  person: Person = {
-    id: 1,
-    name: 'William Sanders'
+  // person: Person = {
+  //   id: 1,
+  //   name: 'William Sanders'
+  // }
+  persons = PERSONS;
+  selectedPerson?: Person;
+
+  onSelect(person: Person): void {
+    this.selectedPerson = person;
   }
 }
